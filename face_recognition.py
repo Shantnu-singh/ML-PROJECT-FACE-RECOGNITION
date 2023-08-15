71656861452789d1d7f6a618ba090918eeef3f66
Shantnu-singh <98252196+Shantnu-singh@users.noreply.github.com>
2023-08-15 20:47:07 +0530

    Add files via upload

---
+++ face_recognition.py
@@ -0,0 +1,94 @@
+import face_recognition
+import os, sys
+import cv2
+import numpy as np
+import math
+
+class FaceRecognition:
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    known_face_encodings = []
+    known_face_names = []
+    process_current_frame = True
+
+    def __init__(self):
+        self.encode_faces()
+
+    def encode_faces(self):
+        for image in os.listdir('faces'):
+            face_image = face_recognition.load_image_file(f"faces/{image}")
+            face_encoding = face_recognition.face_encodings(face_image)[0]
+
+            self.known_face_encodings.append(face_encoding)
+            self.known_face_names.append(image)
+        print(self.known_face_names)
+
+    def run_recognition(self):
+        video_capture = cv2.VideoCapture(0)
+
+        if not video_capture.isOpened():
+            sys.exit('Video source not found...')
+
+        while True:
+            ret, frame = video_capture.read()
+
+            # to avoid all frame
+            if self.process_current_frame:
+                # reducing size of frame
+                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                rgb_small_frame = small_frame[:, :, ::-1]
+
+                # Find all the faces and face encodings in the current frame of video
+                self.face_locations = face_recognition.face_locations(rgb_small_frame)
+                self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+
+                self.face_names = []
+                for face_encoding in self.face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                    name = "Unknown"
+                
+
+                    # Calculate the shortest distance to face
+                    face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = self.known_face_names[best_match_index]
+                        
+
+                    self.face_names.append(f'{name}')
+
+            self.process_current_frame = not self.process_current_frame
+
+            # Displaying the results
+            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                # Changing the size of frame to original
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Create the frame with the name
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 0, 255), 1)
+
+            # Display the resulting image
+            cv2.imshow('Face Recognition', frame)
+
+            # To exit 
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        # freeing web-cam
+        video_capture.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    fr = FaceRecognition()
+    fr.run_recognition()
